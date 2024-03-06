811fcc644cabe36f063a4d4ef21cbbefb61ddc25
Erwan <erwan.sorroche@epfedu.fr>
2024-03-06 14:51:38 +0100

    fin du CSS Loader

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,24 +1,27 @@
-import { Router } from "./app/scripts/router";
-import { WelcomeComponent } from "./app/scripts/welcome";
-import { GameComponent } from "./app/scripts/game";
-import { ScoreComponent } from "./app/scripts/score";
+import {Router} from "./app/scripts/router";
+import {WelcomeComponent} from "./app/scripts/welcome";
+import {GameComponent} from "./app/scripts/game";
+import {ScoreComponent} from "./app/scripts/score";
+
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "./app/styles/style.css";
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
 
 router.register("", {
-        component: WelcomeComponent,
-        templateUrl: "/src/app/views/welcome.html",
-         });
+    component: WelcomeComponent,
+    templateUrl: "/src/app/views/welcome.html",
+});
 router.register("/", {
-        component: WelcomeComponent,
-        templateUrl: "/src/app/views/welcome.html",
-         });
+    component: WelcomeComponent,
+    templateUrl: "/src/app/views/welcome.html",
+});
 router.register("game", {
-            component: GameComponent,
-            templateUrl: "/src/app/views/game.html",
-        });
+    component: GameComponent,
+    templateUrl: "/src/app/views/game.html",
+});
 router.register("score", {
-            component: ScoreComponent,
-            templateUrl: "/src/app/views/score.html",
-        });
+    component: ScoreComponent,
+    templateUrl: "/src/app/views/score.html",
+});
